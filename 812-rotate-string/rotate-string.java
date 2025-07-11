4a0feae2769ccfbf80cc3ad800080d8a25ad1856
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-07-11 10:33:55 +0530

    Time: 1 ms (22.49%) | Memory: 42.2 MB (7.87%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,7 +1,18 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()) return false;
-       
-        return (s + s).contains(goal);
+         if(s.length()!=goal.length()){
+        return false;
+     }
+     for(int i=0;i<s.length();i++){
+
+
+        String result=s.substring(i)+s.substring(0,i);
+        if(goal.equals(result)){
+            return true;     
+               }
+     }
+     return false;
     }
 }
+        
+        
