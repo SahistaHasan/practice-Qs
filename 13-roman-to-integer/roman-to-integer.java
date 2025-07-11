6c5ca810a938eede1f7b121fc2bb61ac8ca10af3
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-07-11 11:30:50 +0530

    Time: 3 ms (79.37%) | Memory: 44.7 MB (68.19%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,35 +1,52 @@
 class Solution {
+    
+
+        
     public int romanToInt(String s) {
-        Map<Character,Integer> map = new HashMap<>();
-       map.put('I',1);
-       map.put('V',5);
-       map.put('X',10);
-       map.put('L',50);
-       map.put('C',100);
-       map.put('D',500);
-       map.put('M',1000);
+
+        
        int res=0;
        int i=0;
-       if(s.length()==1) return map.get(s.charAt(0));
+       if(s.length()==1) return map(s.charAt(0));
       while(i<s.length()-1){
-        if(map.get(s.charAt(i))<map.get(s.charAt(i+1))){
-            res=res+(map.get(s.charAt(i+1))-map.get(s.charAt(i)));
+        if(map(s.charAt(i))<map(s.charAt(i+1))){
+            res=res+(map(s.charAt(i+1))-map(s.charAt(i)));
             i=i+2;
             if(i==s.length()-1){
-                res=res+map.get(s.charAt(i));
+                res=res+map(s.charAt(i));
                 return res;
             }
         }
         else{
             if(i==s.length()-2){
-                res=res+map.get(s.charAt(i))+map.get(s.charAt(i+1));
+                res=res+map(s.charAt(i))+map(s.charAt(i+1));
                 return res;
             }
-           else{res=res+(map.get(s.charAt(i)));
+           else{res=res+(map(s.charAt(i)));
            i++;
            }
         }
        }
        return res;
     }
+
+     public int map(char ch) {
+        if (ch == 'I') {
+            return 1;
+        } else if (ch == 'V') {
+            return 5;
+        } else if (ch == 'X') {
+            return 10;
+        } else if (ch == 'L') {
+            return 50;
+        } else if (ch == 'C') {
+            return 100;
+        } else if (ch == 'D') {
+            return 500;
+        } else if (ch == 'M') {
+            return 1000;
+        } else {
+            return 0;
+        }
+}
 }
