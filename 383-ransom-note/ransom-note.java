b55cba5e94238891c60304300c8f68e79d158ced
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-07-26 16:47:21 +0530

    Time: 4 ms (59.23%) | Memory: 44.6 MB (83.94%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<ransomNote.length();i++){
+            map.put(ransomNote.charAt(i),map.getOrDefault(ransomNote.charAt(i),0)+1);
+        }
+        for(int i=0;i<magazine.length();i++){
+            if(map.containsKey(magazine.charAt(i))){
+                if(map.get(magazine.charAt(i))==1){
+                    map.remove(magazine.charAt(i));
+                }
+                else{
+                map.put(magazine.charAt(i),map.get(magazine.charAt(i))-1);
+                }
+            }
+             if(map.size()==0) return true;
+        }
+        return false;
+    }
+}
