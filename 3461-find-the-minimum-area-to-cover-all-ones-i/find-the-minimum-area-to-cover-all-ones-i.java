ec8b1b3aa48835833fa7ac097dc7b7c3181d1508
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-08-22 07:42:10 +0530

    Time: 1 ms (100.00%) | Memory: 197.1 MB (34.15%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.java
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minimumArea(int[][] grid) {
-        int left = grid[0].length;
+        int left = grid[0].length-1;
         boolean found = false;
         for(int j=0;j<grid[0].length;j++){
             for(int i=0;i<grid.length;i++){
@@ -13,7 +13,7 @@ class Solution {
             if(found==true) break;
         }
         found=false;
-        int right=-1;
+        int right=0;
         for(int j=grid[0].length-1;j>=0;j--){
             for(int i=0;i<grid.length;i++){
                 if(grid[i][j]==1) {
@@ -25,7 +25,7 @@ class Solution {
             if(found==true) break;
         }
         found=false;
-        int top=grid.length;
+        int top=grid.length-1;
          for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==1) {
@@ -37,7 +37,7 @@ class Solution {
             if(found) break;
         }
         found=false;
-         int bottom =-1;
+         int bottom =0;
         for(int i=grid.length-1;i>=0;i--){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==1) {
