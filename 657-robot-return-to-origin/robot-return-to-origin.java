c6a97b77c886842e35ac08709a6818365546085a
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-08-27 07:25:17 +0530

    Time: 8 ms (27.62%) | Memory: 44.5 MB (81.48%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int up=0;
+        int right=0;
+        for(int i=0;i<moves.length();i++){
+         if(moves.charAt(i)=='U'){
+            up++;
+         }
+         else if(moves.charAt(i)=='D'){
+            up--;
+         }
+         else if(moves.charAt(i)=='R'){
+            right++;
+         }
+         else if(moves.charAt(i)=='L'){
+            right--;
+         }
+        }
+        if(up==0 && right==0) return true;
+        else return false;
+    }
+}
