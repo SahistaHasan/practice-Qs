8ef2c8316819dd98f588fb692144de090c37ee19
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-08-29 15:42:39 +0530

    Time: 8 ms (30.46%) | Memory: 41 MB (69.24%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int i=1;
+        while(n>=0){
+            n=n-i;
+            if(n<0) return i-1;
+            i++;
+        }
+        return i;
+    }
+}
