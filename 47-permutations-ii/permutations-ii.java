246c6e94089272ebe2dd3fdcd9f6aac882df4719
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-09-19 17:39:01 +0530

    Time: 1 ms (99.89%) | Memory: 45.3 MB (33.40%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    List<List<Integer>> list=new ArrayList<>();
+    public void recurse(int index,int[] nums, boolean [] vis, List<Integer> inner){
+        if(inner.size()==nums.length){
+            list.add(new ArrayList<>(inner));
+            return;
+        }
+        
+       int i=0;
+        while(i<nums.length){
+            if(i!=0 && i<nums.length && nums[i]==nums[i-1] && !vis[i - 1]){
+                i++;
+                continue;
+            }
+            if(i<nums.length && vis[i]==false){
+                vis[i]=true;
+                inner.add(nums[i]);
+                recurse(i,nums,vis,inner);
+                inner.remove(inner.size()-1);
+                vis[i]=false;
+            }
+            i++;
+        }
+      
+        
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> inner = new ArrayList<>();
+        boolean [] vis=new boolean [nums.length];
+        recurse(0,nums,vis,inner);
+        return list;
+    }
+}
