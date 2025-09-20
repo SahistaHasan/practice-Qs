930fe2055105f16ff449c312c214c57e17bfe12e
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-09-20 17:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (99.19%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
@@ -2,14 +2,18 @@ class Solution {
     public String toHex(int num) {
         if (num == 0) return "0";
 
-        char[] hexChars = "0123456789abcdef".toCharArray(); 
-        StringBuilder sb = new StringBuilder();
-        while (num != 0) {
-            int rem = num & 15;
-            sb.append(hexChars[rem]);
-            num >>>= 4;
+        StringBuilder hex = new StringBuilder();
+        long n = num & 0xFFFFFFFFL; // treat as unsigned 32-bit
+
+        while (n > 0) {
+            long rem = n % 16;
+            char hexChar;
+            if (rem < 10) hexChar = (char)('0' + rem);
+            else hexChar = (char)('a' + (rem - 10));
+            hex.insert(0, hexChar);
+            n /= 16;
         }
 
-        return sb.reverse().toString();
+        return hex.toString();
     }
 }
