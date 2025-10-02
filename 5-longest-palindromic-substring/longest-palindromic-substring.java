b70b15f1a7b64eef50ee17a50df46d0e4ccc6583
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-10-02 07:33:22 +0530

    Time: 18 ms (53.81%) | Memory: 45.6 MB (20.67%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,26 +1,24 @@
 class Solution {
-    public boolean recurse(int i, int j, String s,Boolean[][] memo){
-       if(i>=j) return true;
-       if(memo[i][j]!=null) return memo[i][j];
-       if(s.charAt(i)!=s.charAt(j)) return memo[i][j]=false;
-       return memo[i][j]=recurse(i+1,j-1,s,memo);
+    public String expand(int i, int j, String s){
+        if(i<0 || j>=s.length()) return "";
+        while (i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){
+            i--;
+            j++;
+        }
+        return s.substring((i+1),j);
     }
     public String longestPalindrome(String s) {
-        int start=0;
-        int end=0;
-        int maxlen=0;
-        Boolean [][] memo=new Boolean[s.length()][s.length()];
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(recurse(i,j,s,memo)){
-                    if(j-i+1>maxlen){
-                         maxlen=j-i+1;
-                         start=i;
-                         end=j;
-                    }
-                }
-            }
-        }
-        return s.substring(start,end+1);
+       String longest="";
+       for(int i=0;i<s.length();i++){
+        String s1=expand(i,i,s);
+        String s2=expand(i,i+1,s);
+        
+        String currLongest = s1.length() > s2.length() ? s1 : s2;
+            
+            if (currLongest.length() > longest.length()) {
+                longest = currLongest;
+       }
+       }
+       return longest;
     }
 }
