858c720448936e1562372e7e61f05ad22f4d503d
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-10-30 05:08:12 +0530

    Time: 1 ms (99.68%) | Memory: 44.5 MB (27.66%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int compress(char[] chars) {
+        int i=0;
+        int index=0;
+        
+        while(i<chars.length){
+            int count=0;
+            char curr= chars[i];
+            while(i<chars.length && chars[i]==curr){
+               count++;
+               i++;
+            }
+            chars [index++]=curr;
+            
+            if(count>1){
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+            
+        }
+        return index;
+    }
+}
