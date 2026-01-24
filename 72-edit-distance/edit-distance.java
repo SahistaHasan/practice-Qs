08b4dead92b8384451da5801787a4296b346859a
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2026-01-24 07:27:24 +0530

    Time: 6 ms (10.26%) | Memory: 47.2 MB (43.28%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,28 +1,48 @@
 class Solution {
-    public int solve(String s1, String s2, int index1, int index2, int [][] dp){
-        if(index1<0) return index2+1;
-        if(index2<0) return index1+1;
-        if(dp[index1][index2]!=-1) return dp[index1][index2];
-        int match=Integer.MAX_VALUE;
-        int notmatch1=Integer.MAX_VALUE;
-        int notmatch2=Integer.MAX_VALUE;
-        int notmatch3=Integer.MAX_VALUE;
-        if(s1.charAt(index1)==s2.charAt(index2)){
-             match=solve(s1,s2,index1-1,index2-1,dp);
+    // public int solve(String s1, String s2, int index1, int index2, int [][] dp){
+    //     if(index1<0) return index2+1;
+    //     if(index2<0) return index1+1;
+    //     if(dp[index1][index2]!=-1) return dp[index1][index2];
+    //     int match=Integer.MAX_VALUE;
+    //     int notmatch1=Integer.MAX_VALUE;
+    //     int notmatch2=Integer.MAX_VALUE;
+    //     int notmatch3=Integer.MAX_VALUE;
+    //     if(s1.charAt(index1)==s2.charAt(index2)){
+    //          match=solve(s1,s2,index1-1,index2-1,dp);
+    //     }
+    //     else{
+    //         notmatch1=1+solve(s1,s2,index1,index2-1,dp); //insert
+    //         notmatch2=1+solve(s1,s2,index1-1,index2,dp); //delete
+    //         notmatch3=1+solve(s1,s2,index1-1,index2-1,dp); //replace
+    //     }
+    //     return dp[index1][index2]=Math.min(match,Math.min(notmatch1,Math.min(notmatch2,notmatch3)));
+
+    // }
+    public int solve(String s1, String s2,int [][] dp){
+        for(int i=0;i<=s2.length();i++){
+            dp[0][i]=i;
         }
-        else{
-            notmatch1=1+solve(s1,s2,index1,index2-1,dp); //insert
-            notmatch2=1+solve(s1,s2,index1-1,index2,dp); //delete
-            notmatch3=1+solve(s1,s2,index1-1,index2-1,dp); //replace
+        for(int i=0;i<=s1.length();i++){
+            dp[i][0]=i;
         }
-        return dp[index1][index2]=Math.min(match,Math.min(notmatch1,Math.min(notmatch2,notmatch3)));
+        for(int i=1;i<=s1.length();i++){
+            for(int j=1;j<=s2.length();j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
 
     }
     public int minDistance(String word1, String word2) {
         int [][] dp = new int[word1.length()+1][word2.length()+1];
         for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],Integer.MAX_VALUE);
         }
-        return solve(word1,word2,word1.length()-1,word2.length()-1,dp);
+        return solve(word1,word2,dp);
     }
 }
