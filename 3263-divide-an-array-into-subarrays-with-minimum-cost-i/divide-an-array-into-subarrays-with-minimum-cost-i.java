e5a5f05bfe340e8c85dc01bee52c67b4f12c7be8
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2026-02-01 06:55:33 +0530

    Time: 5 ms (22.80%) | Memory: 46.1 MB (10.40%) - LeetSync

---
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimumCost(int[] nums) {
+       int [] arr = new int[nums.length-1];
+       for(int i=1;i<nums.length;i++){
+        arr[i-1]=nums[i];
+       }
+       Arrays.sort(arr);
+       return nums[0]+arr[0]+arr[1];
+    }
+}
