200e37eab5805f9e984c3608f1b292012733816e
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2026-02-04 19:42:43 +0530

    Time: 9 ms (86.52%) | Memory: 43.6 MB (66.48%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    int mod = 1_000_000_007;
+
+    public int solve(int n, int k, int target, int [][] dp){
+        if(n==0 && target==0) return 1;
+        if(n==0 || target<=0) return 0;
+        if(dp[n][target]!=-1) return dp[n][target];
+        int ways=0;
+        for(int i=1;i<=k;i++){
+             ways=(ways+solve(n-1,k,target-i,dp))%mod;
+        }
+        return dp[n][target]=ways%mod;
+    }
+    public int numRollsToTarget(int n, int k, int target) {
+        int [][] dp = new int[n+1][target+1];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(n,k,target,dp);
+    }
+}
